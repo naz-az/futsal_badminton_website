2dd22c5a1200a709c10b7975a70d9d31c298daad
naz-az <nazzazlan93@gmail.com>
2024-02-12 22:04:25 +0800

    i

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['kickmates.onrender.com']
+ALLOWED_HOSTS = ['ALLOWED_HOSTS']
 
 # , '192.168.0.102'
 # Application definition
