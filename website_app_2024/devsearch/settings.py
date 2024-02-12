6fbcb69e9092427059d2a09e55e64ac8ef2f0098
naz-az <nazzazlan93@gmail.com>
2024-02-12 13:12:25 +0800

    s

--- website_app_2024/devsearch/settings.py
+++ website_app_2024/devsearch/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # , '192.168.0.102'
 # Application definition
